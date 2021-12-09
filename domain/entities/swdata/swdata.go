4984c648a661c0fb9be2dc57da0c457397a3aca7
SousaGLucas <lucasgsousa93@gmail.com>
2021-12-09 13:30:11 -0300

    creation of the entities package

---
+++ domain/entities/swdata/swdata.go
@@ -0,0 +1,12 @@
+package swdata
+
+// package responsible for mirror database data
+
+type Data struct {
+	People    map[string]interface{}
+	Planets   map[string]interface{}
+	Films     map[string]interface{}
+	Species   map[string]interface{}
+	Vehicles  map[string]interface{}
+	Starships map[string]interface{}
+}
